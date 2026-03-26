662d4a79c2a26a7dd020b0e3805c2d0bbbb2e2e1
Sony23fd <snnbyrdv@gmail.com>
2026-03-26 23:43:46 +0800

    fix ts build error

--- test-anar.ts
+++ test-anar.ts
@@ -1,8 +1,8 @@
 import * as fs from 'fs';
 const data = JSON.parse(fs.readFileSync('anar.txt', 'utf8'));
 const orders = data.data.orders;
-orders.forEach(b => {
-  b.orderitemSet.forEach(item => {
+orders.forEach((b: any) => {
+  b.orderitemSet.forEach((item: any) => {
     if (item.name && item.name.toLowerCase().includes('zulaa')) {
       console.log('FOUND ZULAA:', item.name, 'PHONE:', item.phoneNumber);
     }
